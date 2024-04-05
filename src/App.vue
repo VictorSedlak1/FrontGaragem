5ca2240ce0467d470be7eb72213e6366bf677375
VictorSedlak1 <victorsedlak04@gmail.com>
2024-04-05 10:12:50 -0300

    Criação do front Garagem

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import router from './router';
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
     <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/marcas">Marcas</router-link> 
+    <router-link to="/cores">Cores</router-link> 
+    <router-link to="/acessorios">Acessorios</router-link> 
   </header>
   <main>
     <RouterView />
